702f0116a00f2be8056e4a479ab0835997bac09b
Yuki Nagae <yuki.nagae1130@gmail.com>
2019-10-20 22:04:59 +0900

    WIP

--- gc.c
+++ gc.c
@@ -4,8 +4,38 @@
 /*  mini_gc_malloc                                                                      */
 /* ========================================================================== */
 
-#define PTRSIZE ((size_t)sizeof(void *))
+#define TINY_HEAP_SIZE 0x4000 // 16384 (2 ** 14)
+// size_t: unsigned long
+#define PTRSIZE ((size_t)sizeof(void *)) // unsigned long: 8 bytes (64 bits)
+#define HEAP_LIMIT 10000
 
+// TODO: why?
+// x = 10
+// a = 8
+// (((10) + (8 - 1)) & ~(8 - 1))
+// ((10) + (7)) & ~(7)
+// (1010 + 111) & ~(111)
+// (10001) & ~(111)
+// (10001) & 11000
+// 10000 => 16
+//
+// x = 16
+// a = 8
+// (((16) + (8 - 1)) & ~(8 - 1))
+// ((16) + (7)) & ~(7)
+// (10000 + 111) & ~(111)
+// (10111) & ~(111)
+// (10111) & 11000
+// 10000 => 16
+//
+// x = 17
+// a = 8
+// (((17) + (8 - 1)) & ~(8 - 1))
+// ((17) + (7)) & ~(7)
+// (10001 + 111) & ~(111)
+// (11000) & ~(111)
+// (11000) & 11000
+// 11000 => 24
 #define ALIGN(x, a) (((x) + (a - 1)) & ~(a - 1))
 
 typedef struct header
@@ -15,16 +45,40 @@ typedef struct header
     struct header *next_free;
 } Header;
 
+static Header *free_list;
+static size_t gc_heaps_used = 0;
+
+// TODO: not yet
+static Header *add_heap(size_t req_size)
+{
+    //
+    void *p;
+    Header *aligh_p;
+}
+
+// TODO: not yet
 void *mini_gc_malloc(size_t req_size)
 {
     Header *p, *prevp;
     size_t do_gc = 0;
 
-    printf("%zu\n", PTRSIZE);
-
     req_size = ALIGN(req_size, PTRSIZE);
 
-    printf("%zu\n", req_size);
+    if (req_size <= 0)
+    {
+        return NULL;
+    }
+
+    if ((prevp = free_list) == NULL)
+    {
+        if (!(p = add_heap(TINY_HEAP_SIZE)))
+        {
+            return NULL;
+        }
+        prevp = free_list = p;
+    }
+
+    // TODO: not yet implemented
 
     return NULL;
 }
@@ -35,30 +89,28 @@ void *mini_gc_malloc(size_t req_size)
 
 static void *stack_start = NULL; // void pointer
 
+// TODO: not yet
 // Initialize GC stack start
 void gc_init(void)
 {
-    long dummy;
-
-    dummy = 42;
-
-    stack_start = ((void *)&dummy);
-
-    printf("%p\n", stack_start);
+    long dummy = 42;
+    stack_start = (void *)&dummy;
+    printf("%p\n", stack_start); // TODO: debug
 }
 
 /* ========================================================================== */
 /*  test                                                                      */
 /* ========================================================================== */
 
+// TODO: not yet
 static void test_mini_gc_malloc_free(void)
 {
     void *p1, *p2, *p3;
 
     /* malloc check */
     p1 = (void *)mini_gc_malloc(10);
-    p2 = (void *)mini_gc_malloc(16);
-    p3 = (void *)mini_gc_malloc(17);
+    // p2 = (void *)mini_gc_malloc(16);
+    // p3 = (void *)mini_gc_malloc(17);
 }
 
 static void test_garbage_collect(void)
